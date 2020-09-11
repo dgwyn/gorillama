4583b01af328c380d314ece9d7b0d4207f8dc6d5
David Nghiem <lamda@defn.sh>
2020-09-10 17:36:03 -0700

    use module github.com/defn/shlib/v2

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 
+	"github.com/defn/shlib/v2"
 	"github.com/dgwyn/gorillama"
 )
 
 func main() {
 	fmt.Printf("1 + 2 = %d\n", gorillama.Sum(1, 2))
+	fmt.Println(shlib.Hello())
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dgwyn/gorillama
 
 go 1.14
+
+require github.com/defn/shlib/v2 v2.0.1
